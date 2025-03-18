b7e44c07fc4077358ea44d62dc75954c4ab05b1c
TonyGrif <TonyGriffin2000@gmail.com>
2025-03-18 10:40:56 -0400

    build(uv): init project with uv

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1 align="center"> :ship: Titanic :ship: </h1>
+Yet another EDA and ML model predicting Titanic survival
+
+## Requirements
+* [uv](https://github.com/astral-sh/uv) for package management
---
+++ flake.nix
@@ -0,0 +1,81 @@
+{
+  description = "Flake using uv2nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      uv2nix,
+      pyproject-nix,
+      pyproject-build-systems,
+      ...
+    }:
+    let
+      inherit (nixpkgs) lib;
+
+      workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+
+      overlay = workspace.mkPyprojectOverlay {
+        sourcePreference = "wheel";
+      };
+
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      python = pkgs.python312;
+
+      # Construct package set
+      pythonSet =
+        # Use base package set from pyproject.nix builders
+        (pkgs.callPackage pyproject-nix.build.packages {
+          inherit python;
+        }).overrideScope
+          (
+            lib.composeManyExtensions [
+              pyproject-build-systems.overlays.default
+              overlay
+            ]
+          );
+
+    in
+    {
+      packages.x86_64-linux.default = pythonSet.mkVirtualEnv "titanic" workspace.deps.default;
+
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = [
+          python
+          pkgs.uv
+        ];
+        env =
+          {
+            UV_PYTHON_DOWNLOADS = "never";
+            UV_PYTHON = python.interpreter;
+          }
+          // lib.optionalAttrs pkgs.stdenv.isLinux {
+            LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
+          };
+        shellHook = ''
+          unset PYTHONPATH
+        '';
+      };
+    };
+}
