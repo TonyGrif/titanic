f16bdd907cebbda298c25e1540aecf140f30ccc0
TonyGrif <TonyGriffin2000@gmail.com>
2025-06-28 21:52:27 -0400

    build(nix): add flake with jupyter

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    nixpkgs-unstable,
+    ...
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in {
+    formatter.${system} = nixpkgs.legacyPackages.${system}.alejandra;
+
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        (python312.withPackages (ps:
+          with ps; [
+            jupyterlab
+          ]))
+      ];
+    };
+  };
+}
